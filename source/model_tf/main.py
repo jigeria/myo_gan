2895a9b30e92b7ad5f9a29c7484a17747b97c3e8
MagmaTart <magmatart@gmail.com>
2018-04-26 08:10:33 +0900

    [MODIFY] Tensorflow Model

--- source/model_tf/main.py
+++ source/model_tf/main.py
@@ -1,32 +1,50 @@
 import tensorflow as tf
 import numpy as np
+from sklearn.preprocessing import normalize
 import cv2
 # TODO : python-opencv install
 
 from load_data import DataLoader
 from model import Model
 
-loader = DataLoader(emg_data_path='./Sample_data/time_match.csv', image_path='./Sample_data/hand_images/')
+loader = DataLoader(emg_data_path='./Sample_data/emg.csv', image_path='./Sample_data/hand_images/')
 
 # images, emgs = loader.get_next_batch(3)
 #
 # print(type(images), images.shape)
 # print(type(emgs), emgs.shape)
 
+batch_size = 8
+
 sess = tf.Session()
 
-model = Model()
+model = Model(batch_size=8)
 model.build()
 
 sess.run(tf.global_variables_initializer())
 
+# print('Images shape :', images.shape)
+# print('EMGs shape :', emgs.shape)
+# print('Z vector shape :', z.shape)
+
+emgs = loader.get_next_second_emgs(batch_size)
+images = loader.get_next_images(batch_size)
+images = images/127.5
+
 for i in range(10000):
-    image, emg = loader.get_next_batch(1)
-    z = np.random.rand(1, 80)
-    c = np.random.rand(1, 20)
-    # TODO : Check emg data shape
+    print('Iteration ', i)
+    emgs = loader.get_next_second_emgs(batch_size)
+    images = loader.get_next_images(batch_size)
 
-    print(z.shape, c.shape)
+    for i in range(len(emgs)):
+        emgs[i] = normalize(emgs[i])
 
-    _, _, ld, lg = sess.run([model.d_optimizer, model.g_optimizer, model.d_loss, model.g_loss], feed_dict={model.real_image:image, model.z:z, model.c:c})
+    z = np.random.rand(batch_size, 20)
+
+    _, _, ld, lg = sess.run([model.d_optimizer, model.g_optimizer, model.d_loss, model.g_loss], feed_dict={model.real_image:images, model.emg_data:emgs, model.z:z})
     print(ld, lg)
+
+    test = sess.run(model.fake_image, feed_dict={model.emg_data:emgs, model.z:z})
+    print(test.shape)
+    # cv2.imshow('Test', test[0])
+    # cv2.waitKey(100000)
--- source/model_tf/model.py
+++ source/model_tf/model.py
@@ -40,22 +40,33 @@ class Model:
         self.learning_rate = learning_rate
         self.batch_size = batch_size
 
-    def generator(self, emg_data, reuse=False):
-        # input = tf.concat([z, c], 1)
-
-        # LSTM
+    def lstm(self, emg_data):
+        print(emg_data.shape)
         cell = rnn.BasicLSTMCell(num_units=256, state_is_tuple=True)
         initial_state = cell.zero_state(self.batch_size, tf.float32)
         outputs, _states = tf.nn.dynamic_rnn(cell, emg_data, initial_state=initial_state, dtype=np.float32)
 
-        input = tf.reshape(outputs, [-1, 256])
-        input = tflayers.fully_connected(inputs=input, num_outputs=100, activation_fn=None)
+        # 300줄이 순차적으로 RNN에 들어가고, 최종 출력은 outputs의 마지막 row임
+        print(outputs[:, -1].shape)
+        # outputs = tf.reshape(outputs, [-1, 256])
+        outputs = outputs[:, -1]
+        print(outputs.shape)
+        outputs = tflayers.fully_connected(inputs=outputs, num_outputs=80, activation_fn=None)
+        return outputs
+
+    def generator(self, emg_data, z, reuse=False):
+        # (None, 300, 16) -> (1, 100)
+        input = self.lstm(emg_data)
+        print(input.shape)
+        input = tf.concat([input, z],  1)
+        print(input.shape)
 
         # Generator
         net = slim.fully_connected(input, 256, activation_fn=tf.nn.relu,
                                       weights_initializer=tflayers.xavier_initializer(),
                                       biases_initializer=tflayers.xavier_initializer(),
                                       reuse=reuse)
+        print(net)
         net = tf.reshape(net, [-1, 16, 16, 1])
         net = slim.conv2d(net, num_outputs=128, kernel_size=1, stride=1)
         print(net)
@@ -65,19 +76,24 @@ class Model:
                 with slim.arg_scope([slim.batch_norm], decay=0.95, center=True, scale=True,
                                     activation_fn=tf.nn.relu, is_training=(self.mode=='train')):
                     net = slim.conv2d_transpose(net, num_outputs=256)       # output : 32 x 32
+                    print(net)
                     net = slim.batch_norm(net)
                     net = slim.conv2d_transpose(net, num_outputs=512)       # output : 64 x 64
+                    print(net)
                     net = slim.batch_norm(net)
                     net = slim.conv2d_transpose(net, num_outputs=256)       # output : 128 x 128
+                    print(net)
                     net = slim.batch_norm(net)
 
             net = slim.conv2d_transpose(net, num_outputs=1, kernel_size=1, stride=1, padding='VALID',
                                         weights_initializer=tflayers.xavier_initializer())
+            print(net)
 
         return net
 
     def discriminator(self, input, reuse=False):
         net = input
+        print(net)
 
         with tf.variable_scope("discriminator", reuse=reuse):
             with slim.arg_scope([slim.conv2d], kernel_size=2, stride=2, padding='SAME',
@@ -85,36 +101,45 @@ class Model:
                 with slim.arg_scope([slim.batch_norm], decay=0.95, center=True, scale=True,
                                     activation_fn=tf.nn.leaky_relu, is_training=(self.mode=='train')):
                     net = slim.conv2d(net, num_outputs=256)     # 64 x 64
+                    print(net)
                     net = slim.batch_norm(net)
                     net = slim.conv2d(net, num_outputs=512)     # 32 x 32
+                    print(net)
                     net = slim.batch_norm(net)
                     net = slim.conv2d(net, num_outputs=256)     # 16 x 16
+                    print(net)
                     net = slim.batch_norm(net)
                     net = slim.conv2d(net, num_outputs=128)     # 8 x 8
+                    print(net)
                     net = slim.batch_norm(net)
                     net = slim.conv2d(net, num_outputs=128)     # 4 x 4
+                    print(net)
                     net = slim.batch_norm(net)
 
             net = slim.conv2d(net, num_outputs=1, kernel_size=4, stride=1, activation_fn=tf.nn.leaky_relu,
                               weights_initializer=tflayers.xavier_initializer())
+            print(net)
             net = slim.flatten(net)
+            print(net)
 
         return net
 
     def build(self):
         self.real_image = tf.placeholder(tf.float32, [None, 128, 128, 1])
-        self.z = tf.placeholder(tf.float32, [None, 80])
-        self.c = tf.placeholder(tf.float32, [None, 20])
+        self.emg_data = tf.placeholder(tf.float32, [None, 300, 16])
+        self.z = tf.placeholder(tf.float32, [None, 20])
+        # self.c = tf.placeholder(tf.float32, [None, 20])
 
-        self.fake_image = self.generator(self.z, self.c)
+        # self.fake_image = self.generator(self.z, self.c)
+        self.fake_image = self.generator(self.emg_data, self.z, reuse=False)
         self.fake_logits = self.discriminator(self.fake_image)
         self.real_logits = self.discriminator(self.real_image, reuse=True)
 
-        self.d_loss_fake = slim.losses.sigmoid_cross_entropy(self.fake_logits, tf.zeros_like(self.fake_logits))
-        self.d_loss_real = slim.losses.sigmoid_cross_entropy(self.real_logits, tf.ones_like(self.real_logits))
+        self.d_loss_fake = tf.losses.sigmoid_cross_entropy(self.fake_logits, tf.zeros_like(self.fake_logits))
+        self.d_loss_real = tf.losses.sigmoid_cross_entropy(self.real_logits, tf.ones_like(self.real_logits))
         self.d_loss = tf.reduce_mean(self.d_loss_fake + self.d_loss_real)
 
-        self.g_loss = tf.reduce_mean(slim.losses.sigmoid_cross_entropy(self.fake_logits, tf.ones_like(self.fake_logits)))
+        self.g_loss = tf.reduce_mean(tf.losses.sigmoid_cross_entropy(self.fake_logits, tf.ones_like(self.fake_logits)))
 
         self.d_optimizer = tf.train.AdamOptimizer(self.learning_rate).minimize(self.d_loss)
         self.g_optimizer = tf.train.AdamOptimizer(self.learning_rate).minimize(self.g_loss)
